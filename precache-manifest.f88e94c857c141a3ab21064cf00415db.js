460a7c24d2cb3592c58917ca4a8864ccb414f4b6
Skylar-Brockbank <skylar.brockbank@gmail.com>
2022-02-17 11:53:44 -0800

    Updates

---
+++ precache-manifest.f88e94c857c141a3ab21064cf00415db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9299555c12504746c227d5a476f9a7c",
+    "url": "/HangMan/index.html"
+  },
+  {
+    "revision": "b380e8482d83e80d130b",
+    "url": "/HangMan/static/css/main.d87ba40b.chunk.css"
+  },
+  {
+    "revision": "c84ccc2ac369cf3deeda",
+    "url": "/HangMan/static/js/2.48989259.chunk.js"
+  },
+  {
+    "revision": "8730d36f2e0214669fe0",
+    "url": "/HangMan/static/js/3.5bedc685.chunk.js"
+  },
+  {
+    "revision": "b380e8482d83e80d130b",
+    "url": "/HangMan/static/js/main.3cf3f793.chunk.js"
+  },
+  {
+    "revision": "452bf190e22ae05ae941",
+    "url": "/HangMan/static/js/runtime-main.9b2d3d7c.js"
+  }
+]);
